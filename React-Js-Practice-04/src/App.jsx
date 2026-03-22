8a1ac9e79fd766a9dfd67e9835bce8ce9f1fb498
Huzaif Tarazi <huzaiftarazi14@gmail.com>
2026-03-22 15:07:25 +0500

    react-04

--- React-Js-Practice-04/src/App.jsx
+++ React-Js-Practice-04/src/App.jsx
@@ -1,21 +1,23 @@
 import "./App.css";
 import Cards from "./components/Cards.jsx";
 import jobListings from "./components/ObjectData.jsx";
+
 const App = () => {
   return (
     <>
-      {jobListings.map((element, index) => { return(
-        <div className="Parent" key={index}>
-          <Cards
-            companyLogo={element.logo}
-            companyName={element.companyName}
-            daysPosted={element.daysPosted}
-            jobTitle={element.jobTitle}
-            jobTypes={element.jobTypes}
-            salaryPerHour={element.salaryPerHour}
-          />
-        </div>
-        )
+      {jobListings.map((element, index) => {
+        return (
+          <div className="Parent" key={index}>
+            <Cards
+              companyLogo={element.logo}
+              companyName={element.companyName}
+              daysPosted={element.daysPosted}
+              jobTitle={element.jobTitle}
+              jobTypes={element.jobTypes}
+              salaryPerHour={element.salaryPerHour}
+            />
+          </div>
+        );
       })}
     </>
   );
